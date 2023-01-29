98a1e1baf3dc228158f0cbe266d4ee3c059257ef
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2023-01-29 14:53:11 +0900

    プロジェクト間違えたのでファイル更新

--- serverSide.js
+++ serverSide.js
@@ -2,75 +2,29 @@ function doGet () {
   return HtmlService.createHtmlOutputFromFile('index');
 }
 
-function getKadaiFromDB(dayStr) {
-  const sheetName = 'kadaiQuery';
-  let str, sheet, range, values,
+function getCalendarFromDB() {
+  const sheetName = 'calendar';
+
+  let sheet, range, values,
     lock = LockService.getScriptLock();
 
   if (lock.tryLock(5000)) { // 処理が重複した場合、5秒間は他の人が終わるのを待つ
 
-    sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName);
-    range = sheet.getRange(1,1,1,1);
-
-    // まずはqueryシートにqueryを書き、該当データを絞り込む
-    str = '=query(' + "'" + 'kadai' + "'" + '!A:F,';
-    str += '"select A,B,C,D,E where A <= date'+ " '" + dayStr+ "' ";
-    str += 'and B >= date' + " '" + dayStr+ "' ";
-    str += 'and F is null order by B asc",1)';
-
-    range.setValues([[str]]);
-
-    // queryで絞り込まれたデータを取得し、返す。
-    range = sheet.getRange(1,1,sheet.getLastRow(),5); // AからEで7列
-    values = range.getValues()
+    sheet = SpreadsheetApp.openById(calendarSheetId).getSheetByName(sheetName);
+    range = sheet.getRange(1,1,sheet.getLastRow(),5); // AからEで5列
+    values = range.getValues();
 
     lock.releaseLock(); //ロックを解除
-
-    // 驚異！！日付型など、いくつかの種類ものは渡せない！
-    // 仕方がないので文字列へ変換して返す
-    for (let i in values) {
-      if (i != 0) { //最初はタイトルで日付型じゃないから飛ばさないと怒られる
-        values[i][0] = Utilities.formatDate(values[i][0], 'JST', 'yyyy-MM-dd')
-        values[i][1] = Utilities.formatDate(values[i][1], 'JST', 'yyyy-MM-dd')
-      }
-    }
 
   } else {
     // do nothing
   }
 
   return {
-     result : values
+    result : values
   }
 }
 
-function test () {
-
-  putKadaiToDB('2023-01-11', '2023-2-23', '証明マラソン2');
-}
-
-function putKadaiToDB(karaStr, madeStr, naiyou) {
-  const sheetName = 'kadai',
-    userId   = Session.getActiveUser().getUserLoginId();
-  let sheet,
-    lock = LockService.getScriptLock();
-
-  if (lock.tryLock(5000)) { // 処理が重複した場合、5秒間は他の人が終わるのを待つ
-
-    sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName);
-    sheet.appendRow([karaStr, madeStr, naiyou, userId, '=row()']);
-
-    lock.releaseLock(); //ロックを解除
-  } else {
-    // do nothing
-  }
-
-  return {
-    result : 'OK'
-  }
-}
-
-/*
 function getJikanwariFromDB() {
   const sheetName = 'jikanwari';
 
@@ -168,4 +122,3 @@ function getNaiyouSheetName (month) {
   let monthStr = '0' + String(month);
   return ('month' + monthStr.slice(-2)); // 月の数字部分は0埋めで2桁
 }
-*/
