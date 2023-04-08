bb3d36cf2c6b4d124d1add3dd1759438d3a2351c
shirayanagihiroshi <shirayanagi.hiroshi@gmail.com>
2023-04-08 14:49:56 +0900

    だいぶ簡略化してSHRのメモを表示する機能にする

--- serverSide.js
+++ serverSide.js
@@ -2,69 +2,22 @@ function doGet () {
   return HtmlService.createHtmlOutputFromFile('clientSide');
 }
 
-function getCalendarFromDB() {
-  const sheetName = 'calendar';
-
-  let sheet, range, values,
-    lock = LockService.getScriptLock();
-
-  if (lock.tryLock(5000)) { // 処理が重複した場合、5秒間は他の人が終わるのを待つ
-
-    sheet = SpreadsheetApp.openById(calendarSheetId).getSheetByName(sheetName);
-    range = sheet.getRange(1,1,sheet.getLastRow(),5); // AからEで5列
-    values = range.getValues();
-
-    lock.releaseLock(); //ロックを解除
-
-  } else {
-    // do nothing
-  }
-
-  return {
-    result : values
-  }
+function test () {
+  getNaiyouFromDB();
 }
 
-function getJikanwariFromDB() {
-  const sheetName = 'jikanwari';
-
-  let sheet, range, values,
-    lock = LockService.getScriptLock();
-
-  if (lock.tryLock(5000)) { // 処理が重複した場合、5秒間は他の人が終わるのを待つ
-
-    sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName);
-    range = sheet.getRange(1,1,16,7); // (１限から7限+タイトル)*2で16行, AからGで7列
-    values = range.getValues();
-
-    lock.releaseLock(); //ロックを解除
-
-  } else {
-    // do nothing
-  }
-
-  return {
-    result : values
-  }
-}
-
-function getNaiyouFromDB(year, month, day) {
-  const sheetName = 'query';
+// なお、A列はシリアルの日付で、これはそのままではクライアントに返せない
+// B列はA列を文字列にしたもので、これを代わりに返す。
+// 以下でB列からの4列分にしてあるのはそういう意図
+function getNaiyouFromDB() {
+  const sheetName = 'SHRMemo';
   let str, sheet, range, values,
     lock = LockService.getScriptLock();
 
-  if (lock.tryLock(5000)) { // 処理が重複した場合、5秒間は他の人が終わるのを待つ
+  if (lock.tryLock(3000)) { // 処理が重複した場合、3秒間は他の人が終わるのを待つ
 
     sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName);
-    range = sheet.getRange(1,1,1,1);
-
-    // まずはqueryシートにqueryを書き、該当データを絞り込む
-    str = '=query(' + "'" + getNaiyouSheetName(month) + "'" + '!A:G,';
-    str += '"select A,B,C,D,E,F,G where A = ' + String(day) + ' order by B asc, G desc",1)';
-    range.setValues([[str]]);
-
-    // queryで絞り込まれたデータを取得し、返す。
-    range = sheet.getRange(1,1,sheet.getLastRow(),7); // AからGで7列
+    range = sheet.getRange(1,2,366,4); // 1年分とる
     values = range.getValues()
 
     lock.releaseLock(); //ロックを解除
@@ -74,51 +27,6 @@ function getNaiyouFromDB(year, month, day) {
   }
 
   return {
-    res : values
+    result : values
   }
-}
-
-function putNaiyouToDB(year, month, day, koma, kamoku, naiyou, motimono) {
-  const sheetName = 'query',
-    userId   = Session.getActiveUser().getUserLoginId();
-  let nextTuuban, str, sheet, range,
-    sheetNamePut = getNaiyouSheetName(month),
-    lock = LockService.getScriptLock();
-
-  if (lock.tryLock(5000)) { // 処理が重複した場合、5秒間は他の人が終わるのを待つ
-
-    sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetName);
-    range = sheet.getRange(1,1,1,1);
-
-    // まずはqueryシートにqueryを書き、該当の通番の最大値をとる
-    str = '=query(' + "'" + getNaiyouSheetName(month) + "'" + '!A:G,';
-    str += '"select A,B,C,D,E,F,G where A = ' + String(day) + ' and ';
-    str += 'B = ' + String(koma) + ' order by G desc",1)'; // 1は見出し行の数
-    range.setValues([[str]]);
-
-    // queryで絞り込まれたデータを取得する。
-    range = sheet.getRange(2,7); // 通番の先頭
-    nextTuuban = range.getValue();
-    if(nextTuuban == '') {
-      nextTuuban = 1;
-    } else {
-      nextTuuban++;
-    }
-
-    sheet = SpreadsheetApp.openById(sheetId).getSheetByName(sheetNamePut);
-    sheet.appendRow([day, koma, kamoku, naiyou, motimono, userId, nextTuuban]);
-
-    lock.releaseLock(); //ロックを解除
-  } else {
-    // do nothing
-  }
-
-  return {
-    res : nextTuuban
-  }
-}
-
-function getNaiyouSheetName (month) {
-  let monthStr = '0' + String(month);
-  return ('month' + monthStr.slice(-2)); // 月の数字部分は0埋めで2桁
 }
--- sheetIds.js
+++ sheetIds.js
@@ -1,6 +1,3 @@
 // クラスひとつスプレッドシートを用意する。
 // そのため、下のIDはクラス毎に変える必要がある。
-const sheetId = '';
-
-// こらちのスプレッドシートは学校で共通のはず
-const calendarSheetId = '';
+const sheetId = '1USBsjAVMHeCZ_5x0LNT8CkFpclYWxMKYsW0TYGTgVs4'; // 2023-25-kadai-SHRMemo
